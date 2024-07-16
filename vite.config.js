d71ddd717ef78f8bd853b8b3c51883d304fb1b22
Dmitriy <orbetfair1@gmail.com>
2024-07-16 04:40:21 +0300

    Added format script

--- vite.config.js
+++ vite.config.js
@@ -4,28 +4,29 @@ import { ViteMinifyPlugin } from 'vite-plugin-minify';
 import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig({
-    root: './src',
-    server: {
-        port: 3000
+  root: './src',
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './src/index.html',
+      },
     },
-    build: {
-        outDir: '../dist',
-        emptyOutDir: true,
-        rollupOptions: {
-            input:{
-                main:'./src/index.html',
-            }
-        }
-    },
-    plugins: [
-        injectHTML(),
-        ViteMinifyPlugin(),
-        ViteImageOptimizer({
-        png: {
-            quality: 80,
-        },
-        jpg: {
-            quality: 80,
-        },
-    })]
-})
+  },
+  plugins: [
+    injectHTML(),
+    ViteMinifyPlugin(),
+    ViteImageOptimizer({
+      png: {
+        quality: 80,
+      },
+      jpg: {
+        quality: 80,
+      },
+    }),
+  ],
+});
