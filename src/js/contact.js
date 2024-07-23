3bddc728f78a15ccafdbb81e1c8795cf86c877a3
Dmitriy <orbetfair1@gmail.com>
2024-07-23 04:59:58 +0300

    Created contact: hero section

---
+++ src/js/contact.js
@@ -0,0 +1,10 @@
+import '../assets/scss/contact.scss';
+
+// component
+import { useTheme } from './components/theme';
+import { useBurger } from './components/burger';
+import { usePartnersSlider } from './components/home/slider';
+
+useTheme();
+useBurger();
+usePartnersSlider();
