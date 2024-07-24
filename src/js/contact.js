a30c2b7e213d6f4c4c9947eb66bab12a53f5ce18
Dmitriy <orbetfair1@gmail.com>
2024-07-24 04:00:21 +0300

    Created contact: Get Started form-section

--- src/js/contact.js
+++ src/js/contact.js
@@ -4,7 +4,9 @@ import '../assets/scss/contact.scss';
 import { useTheme } from './components/theme';
 import { useBurger } from './components/burger';
 import { usePartnersSlider } from './components/home/slider';
+import { usePhone } from './components/contact/phone';
 
 useTheme();
 useBurger();
 usePartnersSlider();
+usePhone();
